1f315855f6d38b91938ffab24989283816cd18ed
RosyLin <70286853+RosyLin@users.noreply.github.com>
2020-12-04 16:04:01 -0500

    size of window and ball

--- 20fa-5-maxwell-RosyLin/Maxwell.java
+++ 20fa-5-maxwell-RosyLin/Maxwell.java
@@ -19,6 +19,7 @@ public class Maxwell extends JFrame
     
     JPanel midPanel;
     //GameDisplay game;
+    paintDoor game;
     
     JPanel bottomPanel;
     JButton addButton = new JButton("add");
@@ -50,26 +51,23 @@ public class Maxwell extends JFrame
     	//create panels and buttons
     	topPanel = new JPanel();
     	topPanel.setBackground(Color.orange);
+    	//topPanel.setPreferredSize(new Dimension(700,50));
     	tempLeft = new JLabel("Left Temp: ",SwingConstants.LEFT);
     	tempRight = new JLabel("Right Temp: ",SwingConstants.RIGHT);
-    	
-    	window.add(topPanel, BorderLayout.NORTH);
+
     	topPanel.setLayout(new GridLayout(1,2));
     	topPanel.add(tempLeft);
     	topPanel.add(tempRight);
+    	window.add(topPanel, BorderLayout.NORTH);
     	
-    	//middle
-    	midPanel = new JPanel();
-    	midPanel.setBackground(Color.gray);
-    	window.add(midPanel, BorderLayout.CENTER);
-    	
+       	
     	paintDoor game = new paintDoor();
     	window.add(game);
     	
     	//bottom
     	bottomPanel = new JPanel();
     	setBackground(Color.orange);
-    	window.add(bottomPanel, BorderLayout.SOUTH);
+    	window.add(bottomPanel, BorderLayout.PAGE_END);
     	
 		bottomPanel.setLayout(new GridLayout(1,2));
     	bottomPanel.add(resetButton);
@@ -114,8 +112,16 @@ public class Maxwell extends JFrame
             doorAppear = true;
         }
         
+        public boolean isDoorThere()
+        {
+        	return doorAppear;
+        }
+        
         public void paint(Graphics g) 
         {
+        	g.setColor(Color.pink);
+        	g.fillRect(0, 0, 750, 500);
+        	
         	g.setColor(Color.black);
             g.fillRect(362, 0, 10, 150);
             
@@ -137,7 +143,8 @@ public class Maxwell extends JFrame
 		int vx, vy;
 		double oldx, oldy;
 		
-		public Ball() {
+		public Ball() 
+		{
 			x = (int) (Math.random() * 400 + 100); // [100, 500)
 			y = (int) (Math.random() * 400 + 100);
 
@@ -145,15 +152,78 @@ public class Maxwell extends JFrame
 			vy = (int) (Math.random() * 100 - 50);
 		}
 
-		public Ball(int xClick, int yClick) {
+		public Ball(int xClick, int yClick) 
+		{
 			x = xClick;
 			y = yClick;
 
 			vx = (int) (Math.random() * 100 - 50); // [-50, 50)
 			vy = (int) (Math.random() * 100 - 50);
 		}
+		
+		public void move(double delta) 
+		{
+			oldx = x;
+			oldy = y;
+			x += vx * delta;
+			y += vy * delta;
+			stayOnScreen();
+		}
+
+		public void stayOnScreen() 
+		{
+			if (game.isDoorThere())
+			{
+				
+			}
+			// Check bounces off each edge of screen
+			if (x < 0)
+				vx *= -1;
+			if (x > 550)
+				vx *= -1;
+			if (y < 0)
+				vy *= -1;
+			if (y > 550)
+				vy *= -1;
+		}
+		
+		
+		
     }
     
+    public class redBall extends Ball
+    {
+    	redBall() {
+            super();
+        }
+
+        redBall(int x, int y) {
+            super(x, y);
+        }
+
+        public void drawMe ( Graphics g )
+        {
+            g.setColor( Color.RED );
+            g.fillOval( (int)(x-2), (int)(y-2), 5, 5 );
+        }
+    }
+    
+    public class blueBall extends Ball
+    {
+    	blueBall() {
+            super();
+        }
+
+        blueBall(int x, int y) {
+            super(x, y);
+        }
+
+        public void drawMe ( Graphics g )
+        {
+            g.setColor( Color.RED );
+            g.fillOval( (int)(x-2), (int)(y-2), 5, 5 );
+        }
+    }
     
     public static void main(String[] args) 
     {
